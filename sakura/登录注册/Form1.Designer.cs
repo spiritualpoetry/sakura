2644ee67c40774e0aa593da973c0b15f4ffcd312
sakura <2095408981@qq.com>
2023-05-26 17:48:22 +0800

    测试17：48

--- sakura/登录注册/Form1.Designer.cs
+++ sakura/登录注册/Form1.Designer.cs
@@ -41,9 +41,10 @@
             // 
             // Login
             // 
-            this.Login.Location = new System.Drawing.Point(57, 146);
+            this.Login.Location = new System.Drawing.Point(187, 281);
+            this.Login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.Size = new System.Drawing.Size(112, 34);
             this.Login.TabIndex = 0;
             this.Login.Text = "登录";
             this.Login.UseVisualStyleBackColor = true;
@@ -51,9 +52,10 @@
             // 
             // Cancel
             // 
-            this.Cancel.Location = new System.Drawing.Point(162, 146);
+            this.Cancel.Location = new System.Drawing.Point(344, 281);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.Size = new System.Drawing.Size(112, 34);
             this.Cancel.TabIndex = 0;
             this.Cancel.Text = "取消";
             this.Cancel.UseVisualStyleBackColor = true;
@@ -62,41 +64,46 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(55, 61);
+            this.label1.Location = new System.Drawing.Point(183, 154);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.Size = new System.Drawing.Size(50, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "账号";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 91);
+            this.label2.Location = new System.Drawing.Point(183, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.Size = new System.Drawing.Size(50, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "密码";
             // 
             // PWD
             // 
-            this.PWD.Location = new System.Drawing.Point(137, 91);
+            this.PWD.Location = new System.Drawing.Point(307, 198);
+            this.PWD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PWD.Name = "PWD";
             this.PWD.PasswordChar = '*';
-            this.PWD.Size = new System.Drawing.Size(100, 21);
+            this.PWD.Size = new System.Drawing.Size(148, 32);
             this.PWD.TabIndex = 2;
             // 
             // UID
             // 
-            this.UID.Location = new System.Drawing.Point(137, 52);
+            this.UID.Location = new System.Drawing.Point(307, 140);
+            this.UID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UID.Name = "UID";
-            this.UID.Size = new System.Drawing.Size(100, 21);
+            this.UID.Size = new System.Drawing.Size(148, 32);
             this.UID.TabIndex = 2;
             // 
             // SignUp
             // 
-            this.SignUp.Location = new System.Drawing.Point(57, 174);
+            this.SignUp.Location = new System.Drawing.Point(187, 323);
+            this.SignUp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SignUp.Name = "SignUp";
-            this.SignUp.Size = new System.Drawing.Size(75, 23);
+            this.SignUp.Size = new System.Drawing.Size(112, 34);
             this.SignUp.TabIndex = 3;
             this.SignUp.Text = "注册";
             this.SignUp.UseVisualStyleBackColor = true;
@@ -104,9 +111,10 @@
             // 
             // lostpwd
             // 
-            this.lostpwd.Location = new System.Drawing.Point(161, 174);
+            this.lostpwd.Location = new System.Drawing.Point(343, 323);
+            this.lostpwd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lostpwd.Name = "lostpwd";
-            this.lostpwd.Size = new System.Drawing.Size(75, 23);
+            this.lostpwd.Size = new System.Drawing.Size(112, 34);
             this.lostpwd.TabIndex = 4;
             this.lostpwd.Text = "忘记密码";
             this.lostpwd.UseVisualStyleBackColor = true;
@@ -114,9 +122,9 @@
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(248)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(670, 501);
             this.Controls.Add(this.lostpwd);
             this.Controls.Add(this.SignUp);
             this.Controls.Add(this.UID);
@@ -125,9 +133,13 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.Login);
+            this.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- sakura/登录注册/lostform.Designer.cs
+++ sakura/登录注册/lostform.Designer.cs
@@ -31,42 +31,80 @@
             this.EmileText = new System.Windows.Forms.TextBox();
             this.emilelable = new System.Windows.Forms.Label();
             this.submitButton = new System.Windows.Forms.Button();
+            this.YZM = new System.Windows.Forms.Label();
+            this.YZMText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // EmileText
             // 
-            this.EmileText.Location = new System.Drawing.Point(124, 74);
+            this.EmileText.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmileText.Location = new System.Drawing.Point(138, 120);
+            this.EmileText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.EmileText.Name = "EmileText";
-            this.EmileText.Size = new System.Drawing.Size(100, 21);
+            this.EmileText.Size = new System.Drawing.Size(148, 32);
             this.EmileText.TabIndex = 0;
             // 
             // emilelable
             // 
             this.emilelable.AutoSize = true;
-            this.emilelable.Location = new System.Drawing.Point(60, 83);
+            this.emilelable.Location = new System.Drawing.Point(51, 127);
+            this.emilelable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.emilelable.Name = "emilelable";
-            this.emilelable.Size = new System.Drawing.Size(29, 12);
+            this.emilelable.Size = new System.Drawing.Size(44, 18);
             this.emilelable.TabIndex = 1;
             this.emilelable.Text = "邮箱";
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(100, 137);
+            this.submitButton.Location = new System.Drawing.Point(294, 120);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.Size = new System.Drawing.Size(112, 34);
             this.submitButton.TabIndex = 2;
-            this.submitButton.Text = "提交";
+            this.submitButton.Text = "发送";
             this.submitButton.UseVisualStyleBackColor = true;
             this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
+            // YZM
+            // 
+            this.YZM.AutoSize = true;
+            this.YZM.Location = new System.Drawing.Point(312, 82);
+            this.YZM.Name = "YZM";
+            this.YZM.Size = new System.Drawing.Size(62, 18);
+            this.YZM.TabIndex = 3;
+            this.YZM.Text = "label1";
+            this.YZM.Visible = false;
+            // 
+            // YZMText
+            // 
+            this.YZMText.Location = new System.Drawing.Point(138, 179);
+            this.YZMText.Name = "YZMText";
+            this.YZMText.Size = new System.Drawing.Size(100, 28);
+            this.YZMText.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // lostform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(426, 393);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.YZMText);
+            this.Controls.Add(this.YZM);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.emilelable);
             this.Controls.Add(this.EmileText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "lostform";
             this.Text = "lostform";
             this.Load += new System.EventHandler(this.lostform_Load);
@@ -80,5 +118,8 @@
         private System.Windows.Forms.TextBox EmileText;
         private System.Windows.Forms.Label emilelable;
         private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label YZM;
+        private System.Windows.Forms.TextBox YZMText;
+        private System.Windows.Forms.Button button1;
     }
 }
