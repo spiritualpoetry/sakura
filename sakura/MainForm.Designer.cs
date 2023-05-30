a0d4d9bdd60955b0b038c83efd4c82d931354084
sakura <2095408981@qq.com>
2023-05-30 18:02:54 +0800

    主页面设计

--- sakura/MainForm.Designer.cs
+++ sakura/MainForm.Designer.cs
@@ -28,71 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.SETpanel = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.DDpanel = new System.Windows.Forms.Panel();
             this.button7 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.DCpanel = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.childpanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.SETpanel.SuspendLayout();
+            this.DDpanel.SuspendLayout();
+            this.DCpanel.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.panel4);
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Controls.Add(this.SETpanel);
             this.panel1.Controls.Add(this.button8);
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.DDpanel);
             this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.DCpanel);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panelLogo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(10, 0);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(232, 605);
+            this.panel1.Size = new System.Drawing.Size(232, 803);
             this.panel1.TabIndex = 0;
             // 
-            // panel4
+            // SETpanel
             // 
-            this.panel4.Controls.Add(this.button6);
-            this.panel4.Controls.Add(this.button9);
-            this.panel4.Controls.Add(this.button10);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 465);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(232, 94);
-            this.panel4.TabIndex = 14;
+            this.SETpanel.Controls.Add(this.button6);
+            this.SETpanel.Controls.Add(this.button9);
+            this.SETpanel.Controls.Add(this.button10);
+            this.SETpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SETpanel.Location = new System.Drawing.Point(0, 465);
+            this.SETpanel.Name = "SETpanel";
+            this.SETpanel.Size = new System.Drawing.Size(232, 103);
+            this.SETpanel.TabIndex = 14;
             // 
             // button6
             // 
             this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.Location = new System.Drawing.Point(0, 61);
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(0, 67);
             this.button6.Name = "button6";
             this.button6.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.button6.Size = new System.Drawing.Size(232, 34);
+            this.button6.Size = new System.Drawing.Size(232, 36);
             this.button6.TabIndex = 5;
             this.button6.Text = "我的订单";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button9
             // 
             this.button9.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button9.Location = new System.Drawing.Point(0, 27);
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(0, 33);
             this.button9.Name = "button9";
             this.button9.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
             this.button9.Size = new System.Drawing.Size(232, 34);
@@ -100,22 +111,26 @@
             this.button9.Text = "我的订单";
             this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
             this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button10.Location = new System.Drawing.Point(0, 0);
             this.button10.Name = "button10";
             this.button10.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.button10.Size = new System.Drawing.Size(232, 27);
+            this.button10.Size = new System.Drawing.Size(232, 33);
             this.button10.TabIndex = 3;
             this.button10.Text = "我的账户";
             this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button8
             // 
             this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Location = new System.Drawing.Point(0, 414);
             this.button8.Name = "button8";
             this.button8.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
@@ -124,19 +139,21 @@
             this.button8.Text = "设置";
             this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
-            // panel3
+            // DDpanel
             // 
-            this.panel3.Controls.Add(this.button7);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 378);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(232, 36);
-            this.panel3.TabIndex = 12;
+            this.DDpanel.Controls.Add(this.button7);
+            this.DDpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DDpanel.Location = new System.Drawing.Point(0, 378);
+            this.DDpanel.Name = "DDpanel";
+            this.DDpanel.Size = new System.Drawing.Size(232, 36);
+            this.DDpanel.TabIndex = 12;
             // 
             // button7
             // 
             this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Location = new System.Drawing.Point(0, 0);
             this.button7.Name = "button7";
             this.button7.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
@@ -145,10 +162,12 @@
             this.button7.Text = "订单状态";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button5
             // 
             this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Location = new System.Drawing.Point(0, 324);
             this.button5.Name = "button5";
             this.button5.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
@@ -157,20 +176,22 @@
             this.button5.Text = "订单页面";
             this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
             // 
-            // panel2
+            // DCpanel
             // 
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 243);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(232, 81);
-            this.panel2.TabIndex = 7;
+            this.DCpanel.Controls.Add(this.button4);
+            this.DCpanel.Controls.Add(this.button3);
+            this.DCpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DCpanel.Location = new System.Drawing.Point(0, 243);
+            this.DCpanel.Name = "DCpanel";
+            this.DCpanel.Size = new System.Drawing.Size(232, 81);
+            this.DCpanel.TabIndex = 7;
             // 
             // button4
             // 
             this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Location = new System.Drawing.Point(0, 40);
             this.button4.Name = "button4";
             this.button4.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
@@ -179,10 +200,12 @@
             this.button4.Text = "校外点餐";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
             this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Location = new System.Drawing.Point(0, 0);
             this.button3.Name = "button3";
             this.button3.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
@@ -191,10 +214,12 @@
             this.button3.Text = "校内点餐";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Location = new System.Drawing.Point(0, 189);
             this.button2.Name = "button2";
             this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
@@ -203,10 +228,12 @@
             this.button2.Text = "点餐";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(0, 115);
             this.button1.Name = "button1";
             this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
@@ -217,30 +244,75 @@
             // 
             // panelLogo
             // 
-            this.panelLogo.BackColor = System.Drawing.Color.White;
+            this.panelLogo.BackColor = System.Drawing.Color.SkyBlue;
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Controls.Add(this.pictureBox1);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(232, 115);
             this.panelLogo.TabIndex = 9;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(232, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1285, 115);
+            this.panel2.TabIndex = 1;
+            // 
+            // childpanel
+            // 
+            this.childpanel.AutoScroll = true;
+            this.childpanel.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.childpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.childpanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.childpanel.Location = new System.Drawing.Point(232, 115);
+            this.childpanel.Name = "childpanel";
+            this.childpanel.Size = new System.Drawing.Size(1285, 612);
+            this.childpanel.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "点个餐先";
+            // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1048, 605);
+            this.ClientSize = new System.Drawing.Size(1517, 803);
+            this.Controls.Add(this.childpanel);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
-            this.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panel1.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.SETpanel.ResumeLayout(false);
+            this.DDpanel.ResumeLayout(false);
+            this.DCpanel.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -248,19 +320,23 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel SETpanel;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel DDpanel;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel DCpanel;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel childpanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- sakura/MainForm.cs
+++ sakura/MainForm.cs
@@ -15,6 +15,7 @@ namespace sakura
         public MainForm()
         {
             InitializeComponent();
+            VBpanel();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -30,6 +31,85 @@ namespace sakura
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void VBpanel()//隐藏子菜单方法
+        {
+            DDpanel.Visible = false;
+            SETpanel.Visible = false;
+            DCpanel.Visible = false;
+        }
+        private void hidesubmenu()
+        {
+            if (DDpanel.Visible==true) 
+            {
+                DDpanel.Visible = false;
+            }
+            if (SETpanel.Visible == true)
+            {
+                SETpanel.Visible = false;
+            }
+            if (DCpanel.Visible == true)
+            {
+                DCpanel.Visible = false;
+            }
+        }
+        private void showsubmenu(Panel submenu) //显示子菜单方法
+        {
+            if (submenu.Visible==false)
+            {
+                hidesubmenu();
+                submenu.Visible = true;
+            }
+            else
+            {
+                submenu.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showsubmenu(DCpanel);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            showsubmenu(DDpanel);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            showsubmenu(SETpanel);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            hidesubmenu();
         }
     }
 }
